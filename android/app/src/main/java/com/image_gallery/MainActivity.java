450fd5073b3a32e97f9624bd202c456f5c4aec8e
Bushra Shaikh <iambushra37@gmail.com>
2021-06-03 18:35:48 +0530

    Add files via upload

---
+++ android/app/src/main/java/com/image_gallery/MainActivity.java
@@ -0,0 +1,25 @@
+package com.image_gallery;
+
+import com.facebook.react.ReactActivity;
+
+// added for image_gallery
+import android.content.res.Configuration;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Image_Gallery";
+  }
+
+  // added for image_gallery
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    getReactInstanceManager().onConfigurationChanged(this, newConfig);
+  }
+}
